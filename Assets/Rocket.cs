fdd87ce46093d17b8a3f8a4cba536167492bb8b3
S&K <sdg4631@gmail.com>
2018-04-04 15:27:35 +0900

    Adding a Touch of Audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -24,14 +26,22 @@ public class Rocket : MonoBehaviour {
         if(Input.GetKey(KeyCode.Space)) // Can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying) // so the audio doesnt layer on top of each other
+            {
+                audioSource.Play();
+            } 
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if(Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward, Time.deltaTime);
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward, Time.deltaTime);
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
